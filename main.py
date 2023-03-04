839256820bd820857463625bdbb6420081b0b235
รัชชานนท์ กุลภัทรากร <koardtep@gmail.com>
2023-03-04 15:31:58 +0700

    Create class Bat in main.py

--- main.py
+++ main.py
@@ -27,9 +27,23 @@ class Background(Widget):
         texture.dispatch(self)
 
 from random import randint
+from kivy.properties import NumericProperty
+
+class Bat(Image):
+    velocity = NumericProperty(0)
+
+    def on_touch_down(self, touch):
+        self.source = "bat2.png"
+        self.velocity = 150
+        super().on_touch_down(touch)
+
+    def on_touch_up(self, touch):
+        self.source = "bat1.png"
+        super().on_touch_up(touch)
 
 class MainApp(App):
     buids = []
+    GRAVITY = 300
 
     def on_start(self):
         Clock.schedule_interval(self.root.ids.background.scroll_textures, 1/60.)
@@ -43,7 +57,7 @@ class MainApp(App):
             buid = Buiding()
             buid.buid_center = randint(96 + 100, self.root.height - 100)
             buid.size_hint = (None, None)
-            buid.pos = (i*distance_between_buids, 96)
+            buid.pos = (Window.width + i*distance_between_buids, 96)
             buid.size = (64, self.root.height - 96)
 
             self.buids.append(buid)
@@ -62,3 +76,5 @@ class MainApp(App):
         if right_most_x <= Window.width - distance_between_buids:
             most_left_buid = self.buids[buid_xs.index(min(buid_xs))]
             most_left_buid.x = Window.width
+
+MainApp().run()
